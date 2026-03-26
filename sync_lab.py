f7c9f5d319e418dc95dadabcab59811150d01572
zbychfish <zszmigiero@gmail.com>
2026-03-26 18:05:49 +0100

    2306.8

--- sync_lab.py
+++ sync_lab.py
@@ -1072,9 +1072,14 @@ def lab4_atap(state):
     )
 
     run_task('install_stap_on_raptor', lambda: t_install_stap_on_raptor(api), state)
-
     
-
+    print("\n ATAP setup for postgres on raptor")
+    subprocess.run(["/opt/guardium/modules/ATAP/current/files/bin/guardctl", "--db-user=postgres", "--db-home=/usr", "--db-user-dir=/var/lib/pgsql", "--db-type=postgres", "--db-instance=postgres", "--db-version=16", "store-conf"], check=True)
+    subprocess.run(["/opt/guardium/modules/ATAP/current/files/bin/guardctl", "authorize-user", "postgres"], check=True)
+    subprocess.run(["systemctl", "stop", "postgresql"], check=True)
+    subprocess.run(["/opt/guardium/modules/ATAP/current/files/bin/guardctl", "--db-instance=postgres", "activate"], check=True)
+    subprocess.run(["systemctl", "start", "postgresql"], check=True)
+    
 
     print("\n" + "=" * 60)
     print("All labs completed!")
