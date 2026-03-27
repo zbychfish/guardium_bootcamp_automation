9dbb6a1853987dadf30ccfbc30d74681b4693de2
zbychfish <zszmigiero@gmail.com>
2026-03-27 20:08:07 +0100

    modified:   sync_lab.py

--- sync_lab.py
+++ sync_lab.py
@@ -1125,7 +1125,8 @@ def t_setup_raptor_to_deploy_etap():
     subprocess.run(["dnf", "-y", "install", "podman-docker", "skopeo"], check=True)
     print("\n Determine the latest ETAP version")
     etap_versions = subprocess.run(["skopeo", "list-tags", "docker://icr.io/guardium/guardium_external_s-tap"], check=True)
-    print(json.loads(etap_versions.stdout))
+    print(type(etap_versions))
+    #print(json.loads(etap_versions.stdout))
 
     exit(0)
 
