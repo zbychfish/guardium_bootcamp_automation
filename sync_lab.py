cc28d646d943cd6f919a7de1cb042e08624c2bac
zbychfish <zszmigiero@gmail.com>
2026-04-05 13:34:34 +0200

    modified:   sync_lab.py

--- sync_lab.py
+++ sync_lab.py
@@ -1737,7 +1737,7 @@ def t_setup_oracle_traffic_generator():
     commands = [
         #{"cmd": ["mkdir", "-p", "/root/gn-trainings/dbtraffic"]},
         #{"cmd": ["/usr/bin/python3.12", "-m", "venv", ".venv"], "cwd" : "/root/gn-trainings/dbtraffic"},
-        {"cmd": ["/root/gn-trainings/dbtraffic/.venv/bin/python3", "-m", "pip", "install", "--upgrade" "pip"], "cwd" : "/root/gn-trainings/dbtraffic"},
+        {"cmd": ["/root/gn-trainings/dbtraffic/.venv/bin/python3", "-m", "pip", "install", "--upgrade", "pip"], "cwd" : "/root/gn-trainings/dbtraffic"},
         {"cmd": ["/root/gn-trainings/dbtraffic/.venv/bin/pip3", "install", "oracledb", "psycopg2_binary", "faker"], "cwd" : "/root/gn-trainings/dbtraffic"},
         {"cmd": ["wget", "https://ibm.box.com/shared/static/dcm5st6jt4w6ippvkz3ka5ebvb47gymi.zip", "-O", "dbtraffic.zip"], "cwd" : "/root/gn-trainings/dbtraffic"},
         {"cmd": ["unzip", "dbtraffic.zip"], "cwd" : "/root/gn-trainings/dbtraffic"}
