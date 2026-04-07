827a0d38aeb9ce1af74340002c871d5cc303d335
zbychfish <zszmigiero@gmail.com>
2026-04-07 15:39:10 +0200

    modified:   sync_lab.py

--- sync_lab.py
+++ sync_lab.py
@@ -986,7 +986,8 @@ def t_start_etap():
         "-e",
         f"STAP_CONFIG_SQLGUARD_0_SQLGUARD_IP={collector_ip}",
         f"-p={listen_port}:8888/tcp",
-        f"icr.io/guardium/guardium_external_s-tap:v{etap_release}"
+        f"icr.io/guardium/guardium_external_s-tap:v{etap_release}",
+        "-q"
     ]
     subprocess.run(etap_command, check=True)
 
